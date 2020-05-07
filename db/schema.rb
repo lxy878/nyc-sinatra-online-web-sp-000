570297391c15d6d704b5e301f53587240d1ce603
Xiaoyi Liu <lxy878@gmail.com>
2020-05-07 12:14:00 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3) do
+ActiveRecord::Schema.define(version: 2) do
 
   create_table "figures", force: :cascade do |t|
     t.string "name"
@@ -21,10 +21,6 @@ ActiveRecord::Schema.define(version: 3) do
     t.string  "name"
     t.integer "figure_id"
     t.integer "year_completed"
-  end
-
-  create_table "titles", force: :cascade do |t|
-    t.string "name"
   end
 
 end
