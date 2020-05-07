ec327b290e5b5a6e4628d76f9da55f2fb412a5c2
Xiaoyi Liu <lxy878@gmail.com>
2020-05-07 12:14:06 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
-
-  create_table "landmarks", force: :cascade do |t|
-    t.string  "name"
-    t.integer "figure_id"
-    t.integer "year_completed"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
